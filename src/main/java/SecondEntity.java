490ceca9de88997a57486768803e03c26e242161
Tomas Lamr <tomas.lamr@pricefx.eu>
2018-11-13 20:20:35 +0100

    no need to reproduce the 2nd issue

--- src/main/java/SecondEntity.java
+++ src/main/java/SecondEntity.java
@@ -4,13 +4,4 @@ import javax.persistence.Entity;
 @Entity
 public class SecondEntity {
 
-	@Basic private Boolean dirty;
-
-	public boolean isDirty() {
-		return dirty != null && dirty;
-	}
-
-	public boolean getDirty() {
-		return isDirty();
-	}
 }
